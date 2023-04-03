c5ac74dc0cba3b5c83ed7b782a0e44f09362d940
Mohammad Jamil Asfihani <mohammadjamilasfihani@gmail.com>
2023-04-03 10:27:16 +0700

    Jawaban dari Jamil

---
+++ sesi1/jamil/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	input := make(map[string]int)
+
+	input["ikhsan"] = 30
+	input["sans"] = 70
+	input["ikh"] = 80
+
+	fmt.Println(GroupingStundentByPoint(input))
+}
+
+func GroupingStundentByPoint(values map[string]int) map[string][]string {
+	result := make(map[string][]string)
+
+	result["lulus"] = []string{}
+	result["tidak_lulus"] = []string{}
+
+	for student, score := range values {
+		if score > 60 {
+			result["lulus"] = append(result["lulus"], student)
+		} else {
+			result["tidak_lulus"] = append(result["tidak_lulus"], student)
+		}
+	}
+
+	return result
+}
