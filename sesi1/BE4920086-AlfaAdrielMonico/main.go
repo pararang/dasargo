8318687a7d9e9be55544923379d46179e47b36d2
Muhammad Ikhsan <pararang@gmail.com>
2023-04-04 23:15:00 +0800

    add test

--- sesi1/BE4920086-AlfaAdrielMonico/main.go
+++ sesi1/BE4920086-AlfaAdrielMonico/main.go
@@ -1,15 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+	"sort"
+)
 
 func main() {
-	a := map[string]int{
-		"Alfa":  90,
-		"Daffa": 20,
-		"Aceng": 30,
+	input := map[string]int{
+		"A": 59,
+		"B": 60,
+		"C": 61,
+		"D": 0,
+		"E": 86,
 	}
 
-	fmt.Println(GroupingStundentByPoint(a))
+	expectedResult := map[string][]string{
+		"lulus": {
+			"C", "E",
+		},
+		"tidak_lulus": {
+			"A", "B", "D",
+		},
+	}
+
+	result := GroupingStundentByPoint(input)
+
+	sort.Strings(result["lulus"])
+	sort.Strings(result["tidak_lulus"])
+	if !reflect.DeepEqual(expectedResult, result) {
+		fmt.Printf("want: %v\ngot : %v\n", expectedResult, result)
+	} else {
+		fmt.Println("Well Done!")
+	}
 }
 
 func GroupingStundentByPoint(siswa map[string]int) map[string][]string {
