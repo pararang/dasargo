ca80507f846d5a5d83b9d817b7a0117090fd89ab
Muhammad Ikhsan <pararang@gmail.com>
2023-04-04 23:35:43 +0800

    add test

--- sesi1/agatha putra/main.go
+++ sesi1/agatha putra/main.go
@@ -1,13 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+	"sort"
+)
 
 func main() {
-	input := make(map[string]int)
-	input["ikhsan"] = 30
-	input["sans"] = 70
-	input["ikh"] = 80
-	fmt.Println(GroupingStundentByPoint(input))
+	input := map[string]int{
+		"A": 59,
+		"B": 60,
+		"C": 61,
+		"D": 0,
+		"E": 86,
+	}
+
+	expectedResult := map[string][]string{
+		"lulus": {
+			"C", "E",
+		},
+		"tidak_lulus": {
+			"A", "B", "D",
+		},
+	}
+
+	result := GroupingStundentByPoint(input)
+
+	sort.Strings(result["lulus"])
+	sort.Strings(result["tidak_lulus"])
+	if !reflect.DeepEqual(expectedResult, result) {
+		fmt.Printf("want: %v\ngot : %v\n", expectedResult, result)
+	} else {
+		fmt.Println("Well Done!")
+	}
 }
 
 func GroupingStundentByPoint(values map[string]int) map[string][]string {
