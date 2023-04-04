91053403c44aed3d9ad97a88dfd3db8a76aa4176
Muhammad Ikhsan <pararang@gmail.com>
2023-04-04 23:28:11 +0800

    add test

--- sesi1/albi_tegar_prayoga/main.go
+++ sesi1/albi_tegar_prayoga/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+	"sort"
+)
 
 func GroupingStudentByPoint(studentGrade map[string]int) map[string][]string {
 	// membuat slice kosong
@@ -26,20 +30,30 @@ func GroupingStudentByPoint(studentGrade map[string]int) map[string][]string {
 }
 
 func main() {
-	// input value kedalam slice
 	input := map[string]int{
-		"ikhsan": 30,
-		"sans":   70,
-		"ikh":    80,
-		"albi":   50,
-		"dela":   70,
-		"ryzal":  90,
-		"tuni":   100,
-		"yola":   80,
+		"A": 59,
+		"B": 60,
+		"C": 61,
+		"D": 0,
+		"E": 86,
 	}
 
-	// variabel student untuk menampung function GroupingstudentByPoint dengan parameter input
-	student := GroupingStudentByPoint(input)
+	expectedResult := map[string][]string{
+		"lulus": {
+			"C", "E",
+		},
+		"tidak_lulus": {
+			"A", "B", "D",
+		},
+	}
 
-	fmt.Println(student)
+	result := GroupingStudentByPoint(input)
+
+	sort.Strings(result["lulus"])
+	sort.Strings(result["tidak_lulus"])
+	if !reflect.DeepEqual(expectedResult, result) {
+		fmt.Printf("want: %v\ngot : %v\n", expectedResult, result)
+	} else {
+		fmt.Println("Well Done!")
+	}
 }
