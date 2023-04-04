0099344f86132bc38e9a444eafd8f937c153bb04
Muhammad Ikhsan <pararang@gmail.com>
2023-04-04 23:36:50 +0800

    add test

--- sesi1/Adhitia - BE5304103/main.go
+++ sesi1/Adhitia - BE5304103/main.go
@@ -2,19 +2,37 @@ package main
 
 import (
 	"fmt"
+	"reflect"
+	"sort"
 )
 
 func main() {
-	grading := map[string]int{
-		"Test":  100,
-		"Test2": 90,
-		"Test3": 70,
-		"Test4": 50,
+	input := map[string]int{
+		"A": 59,
+		"B": 60,
+		"C": 61,
+		"D": 0,
+		"E": 86,
 	}
 
-	hasil := GroupingStudentByPoint(grading)
+	expectedResult := map[string][]string{
+		"lulus": {
+			"C", "E",
+		},
+		"tidak_lulus": {
+			"A", "B", "D",
+		},
+	}
 
-	fmt.Println(hasil)
+	result := GroupingStudentByPoint(input)
+
+	sort.Strings(result["lulus"])
+	sort.Strings(result["tidak_lulus"])
+	if !reflect.DeepEqual(expectedResult, result) {
+		fmt.Printf("want: %v\ngot : %v\n", expectedResult, result)
+	} else {
+		fmt.Println("Well Done!")
+	}
 }
 
 func GroupingStudentByPoint(grading map[string]int) map[string][]string {
